f7130858416338a389b69e971dd56261f73d9f65
Muhammad Irfan Jauhari <kangrykong123@gmail.com>
2026-04-14 11:42:57 +0700

    refactor: replace priestess.ts monolith with modular directory

---
+++ src/clients/priestess/index.ts
@@ -0,0 +1,4 @@
+export { DEFAULT_PERSONA, getPersona, setPersona } from './persona'
+export { clearHistory, getHistory, getLastMessageTime, saveMessage } from './history'
+export { chat, notify } from './chat'
+export type { ChatResult } from './chat'
